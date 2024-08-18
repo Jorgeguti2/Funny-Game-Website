af9ac0b85e5052f0bf81db337c68a3d991e13a9c
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2024-08-17 20:11:43 -0700

    Added functionality to submit button
    If we click submit, we store the input and we clear out the input field for next input.

--- Website/index.js
+++ Website/index.js
@@ -1,1 +1,6 @@
-
+// get user input when they click submit and assign it to variable userInput
+let userInput;
+document.getElementById("userSubmit").onclick = function(){
+    userInput = document.getElementById("inputBox").value;
+    document.getElementById("inputBox").value='';
+}
