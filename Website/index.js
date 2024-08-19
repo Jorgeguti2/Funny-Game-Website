39bc26b1bad5b5f5fc80503ff827cb577ee6a558
SquidCephalopod <jorgeegutierrez56615@gmail.com>
2024-08-18 21:38:25 -0700

    Changed text id name and added if statement.
    Changed text id to "textBox".
    Added first if statement of the "game" of the website.

--- Website/index.js
+++ Website/index.js
@@ -3,9 +3,15 @@ let userInput;
 document.getElementById("userSubmit").onclick = function(){
     userInput = document.getElementById("inputBox").value;
 
+    // Below is the set of if else statements that will be used to drive the functionality of the website "game"
+
     // If statement to test if "password" is entered or not. Case sensitive.
+    // If it is, we move on to the next prompt.
+    // If it is not, we say error and ask them to try again.
     if (userInput == 'password'){
-        alert(userInput)
+        textBox.innerText = "Incorrect, please try again.";
+    }else {
+        alert('Error, please retry.');
     }
 
     // clear out the input field for next input
