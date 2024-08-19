154bb841279d02fc4abba6e271f2f1cef63ce1a7
SquidCephalopod <jorgeegutierrez56615@gmail.com>
2024-08-18 21:31:29 -0700

    Added id to header. Testing if statement working properly.

--- Website/index.js
+++ Website/index.js
@@ -2,7 +2,12 @@
 let userInput;
 document.getElementById("userSubmit").onclick = function(){
     userInput = document.getElementById("inputBox").value;
-    //if 
+
+    // If statement to test if "password" is entered or not. Case sensitive.
+    if (userInput == 'password'){
+        alert(userInput)
+    }
+
     // clear out the input field for next input
     document.getElementById("inputBox").value='';
 }
