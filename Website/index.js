2bc398b05385eedf291a1a0fc3fa5a8c3f746917
Jorge Gutierrez <jorgeegutierrez56615@gmail.com>
2024-08-18 23:35:09 -0700

    Added final prompt

--- Website/index.js
+++ Website/index.js
@@ -17,6 +17,12 @@ document.getElementById("userSubmit").onclick = function(){
         textBox.innerText = "No, the password is wrong.";
         document.getElementById("textBox").style.marginLeft = '550px';
     }
+    // Next if statement tests if "wrong" is entered or not. Case sensitive.
+    // If it is, we move on to the next prompt.
+    else if (userInput == 'wrong' && textBox.innerText == "No, the password is wrong."){
+        textBox.innerText = "Click the smallest dot on the screen.";
+        document.getElementById("textBox").style.marginLeft = '500px';
+    }
     // If no correct input is entered for the respective prompt, we enter 
     else {
         alert('Error, please retry.');
